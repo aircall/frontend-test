a83bae09cd6cb8e75d13d8ed6ea94c5c9bf45e75
neyser zana <neyserj.zana@gmail.com>
2019-03-05 21:50:04 -0500

    implmenting the routing and starting to add state for call log

--- src/containers/CallLog/CallLog.jsx
+++ src/containers/CallLog/CallLog.jsx
@@ -1,7 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = store => ({
+  // add pertinent state here
+  totalMarkets: store.markets.totalMarkets,
+  totalCards: store.markets.totalCards
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
 
 class CallLog extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div></div>
+    )
+  }
 }
 
-export default CallLog;
+export default connect(mapStateToProps, mapDispatchToProps)(CallLog);
