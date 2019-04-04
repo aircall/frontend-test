4dabf123b11532912eb73e250d696bb221b0e7b1
Bouziane <bbelhadi@gmail.com>
2019-04-04 15:39:45 +0200

    update eslint

--- src/Components/DetailsCall/Component.jsx
+++ src/Components/DetailsCall/Component.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
 import 'moment/locale/fr'
-moment.locale('fr')
-moment().format('D MMM YY')
 
 import icoClose from './../../assets/images/listCalls/icon-close.svg'
 import './style.css'
+moment.locale('fr')
+moment().format('D MMM YY')
 
 class DetailsCall extends Component {
   async componentDidMount() {
