52274131404509265c4753137872cd2ec612875b
Elia Maino <eliamaino@gmail.com>
2019-04-28 00:20:54 +0200

    Switch feed based on selection

--- src/app/components/ActivityFeed/ActivityFeed.jsx
+++ src/app/components/ActivityFeed/ActivityFeed.jsx
@@ -5,19 +5,34 @@ import { connect } from 'react-redux'
 import Call from '../Call/Call.jsx'
 import RLCallsFetcher from '../RLCallsFetcher/RLCallsFetcher.jsx'
 // Store
-import { calls } from '../../store/reducers.js'
+import { calls, selectedFeed } from '../../store/reducers.js'
 import { fetchCalls } from '../../store/actions/actions.js'
 // Styles
 import './activity-feed.css'
 
 const mapStateToProps = (state) => {
   return {
-    calls: calls(state)
+    calls: calls(state),
+    selectedFeed: selectedFeed(state)
   }
 }
 
 const mapDispatchToProps = {
   fetchCalls
+}
+
+const getFilterFn = (filter) => {
+  if (filter === 'all') {
+    return (call) => call
+  }
+
+  if (filter === 'archived') {
+    return (call) => call.is_archived
+  }
+
+  if (filter === 'active') {
+    return (call) => !call.is_archived
+  }
 }
 
 /**
@@ -33,12 +48,15 @@ class ActivityFeed extends React.Component {
 
   render() {
     const callsList = []
+    const filterFn = getFilterFn(this.props.selectedFeed)
 
-    this.props.calls.forEach(call => {
-      callsList.push(
-        <Call key={call.id} call={call}/>
-      )
-    })
+    this.props.calls
+      .filter(filterFn)
+      .forEach(call => {
+        callsList.push(
+          <Call key={call.id} call={call}/>
+        )
+      })
   
     return (
       <div className="activity-feed-container">
