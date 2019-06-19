8baddc328ebdb690d1a463c211877b18478c9474
Jerome Louis <jerome.louis.swing@gmail.com>
2019-06-19 18:32:12 +0200

    feat: add new empty List component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.scss';
 import * as React from 'react';
 
 import Header from './Header';
+import List from './List';
 
 const App = () => {
   return (
     <div className='app'>
       <Header/>
-      <main>Some activities should be here</main>
+      <List/>
     </div>
   );
 };
---
+++ src/List.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import * as React from 'react';
+
+const List = () => {
+  return (
+    <main>
+      Some activities should be here
+    </main>
+  );
+};
+
+export default List;
