f9b1c4231c7a3e1d3cfa6f17761cb2266824e5d1
garfieldduck <garfieldduck@gmail.com>
2019-10-27 18:18:48 +0100

    Format the date and the list item

--- src/components/CallItem.jsx
+++ src/components/CallItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { format } from 'date-fns';
 
 const BORDER_COLOR = {
   missed: '#f98798',
@@ -12,7 +13,7 @@ const Wrapper = styled.div`
   background: #fff;
   border: 2px solid ${({ callType }) => BORDER_COLOR[callType] || BORDER_COLOR.default};
   border-radius: 1.5rem;
-  padding: 1rem 2rem;
+  padding: 0.5rem 1rem;
   margin-top: 1rem;
 `;
 
@@ -29,9 +30,8 @@ const CallItem = ({
   return (
     <Wrapper callType={callType}>
       <p>{`${directionIcon} ${to}`}</p>
-      <p>{from}</p>
-      <p>{via}</p>
-      <p>{time}</p>
+      <p>{`${from} via ${via}`}</p>
+      <p>{format(new Date(time), 'dd/MM HH:mm')}</p>
     </Wrapper>
   );
 };
