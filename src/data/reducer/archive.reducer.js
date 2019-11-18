f96a008a5ca82b0e6e2db71a161a52cf0ead72fa
Samrith Shankar <samrith@thetotem.co>
2019-11-19 00:17:32 +0100

    Fix loading issue with archive reducer

--- src/data/reducer/archive.reducer.js
+++ src/data/reducer/archive.reducer.js
@@ -20,8 +20,7 @@ export default {
       archive: {
         ...state.archive,
         [callId]: {
-          loading: true,
-          error: null
+          loading: false
         }
       }
     };
