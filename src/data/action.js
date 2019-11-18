cd1e42ceffcc515ed1767497d157dd87166d2aba
Samrith Shankar <samrith@thetotem.co>
2019-11-19 00:13:33 +0100

    Added action constants

---
+++ src/data/action.js
@@ -0,0 +1,19 @@
+// feed actions
+export const FEED_LOADING = "FEED_LOADING";
+export const FEED_SUCCESS = "FEED_SUCCESS";
+export const FEED_ERROR = "FEED_ERROR";
+
+// detail view actions
+export const DETAIL_VIEW_LOADING = "DETAIL_VIEW_LOADING";
+export const DETAIL_VIEW_SUCCESS = "DETAIL_VIEW_SUCCESS";
+export const DETAIL_VIEW_ERROR = "DETAIL_VIEW_ERROR";
+
+// archive actions
+export const ARCHIVE_LOADING = "ARCHIVE_LOADING";
+export const ARCHIVE_SUCCESS = "ARCHIVE_SUCCESS";
+export const ARCHIVE_ERROR = "ARCHIVE_ERROR";
+
+// reset
+export const RESET_STATE_LOADING = "RESET_STATE_LOADING";
+export const RESET_STATE_SUCCESS = "RESET_STATE_SUCCESS";
+export const RESET_STATE_ERROR = "RESET_STATE_ERROR";
