b9da1d4bd72156db1d15bf0f10beb02b1c11237d
Samrith Shankar <samrith@thetotem.co>
2019-11-19 00:28:28 +0100

    Added detailView reducer

---
+++ src/data/reducer/detail-view.reducer.js
@@ -0,0 +1,51 @@
+import { defaultStore } from "../store";
+
+import {
+  DETAIL_VIEW_LOADING,
+  DETAIL_VIEW_SUCCESS,
+  DETAIL_VIEW_ERROR,
+  DETAIL_VIEW_RESET
+} from "../action";
+
+export default {
+  [DETAIL_VIEW_LOADING](state, { callId }) {
+    return {
+      ...state,
+      detailView: {
+        ...state.detailView,
+        callId,
+        loading: true,
+        error: null
+      }
+    };
+  },
+
+  [DETAIL_VIEW_SUCCESS](state, { data }) {
+    return {
+      ...state,
+      detailView: {
+        ...state.detailView,
+        loading: false,
+        data
+      }
+    };
+  },
+
+  [DETAIL_VIEW_ERROR](state, { error }) {
+    return {
+      ...state,
+      detailView: {
+        ...state.detailView,
+        loading: false,
+        error
+      }
+    };
+  },
+
+  [DETAIL_VIEW_RESET](state) {
+    return {
+      ...state,
+      detailView: { ...defaultStore.detailView }
+    };
+  }
+};
