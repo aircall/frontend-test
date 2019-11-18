e8d6c42cfc9d5860a32d0d6078b0151c0c6d89eb
Samrith Shankar <samrith@thetotem.co>
2019-11-19 00:13:51 +0100

    Added ultra simple archive reducer

---
+++ src/data/reducer/archive.reducer.js
@@ -0,0 +1,42 @@
+import { ARCHIVE_LOADING, ARCHIVE_ERROR, ARCHIVE_SUCCESS } from "../action";
+
+export default {
+  [ARCHIVE_LOADING](state, { callId }) {
+    return {
+      ...state,
+      archive: {
+        ...state.archive,
+        [callId]: {
+          loading: true,
+          error: null
+        }
+      }
+    };
+  },
+
+  [ARCHIVE_SUCCESS](state, { callId }) {
+    return {
+      ...state,
+      archive: {
+        ...state.archive,
+        [callId]: {
+          loading: true,
+          error: null
+        }
+      }
+    };
+  },
+
+  [ARCHIVE_ERROR](state, { callId, error }) {
+    return {
+      ...state,
+      archive: {
+        ...state.archive,
+        [callId]: {
+          loading: false,
+          error
+        }
+      }
+    };
+  }
+};
