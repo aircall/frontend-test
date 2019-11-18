6d33fda1192e3c7222c4f818d49a8ae81778e566
Samrith Shankar <samrith@thetotem.co>
2019-11-19 00:50:59 +0100

    Properly wire Store context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 
 import { defaultStore } from "../data/store";
@@ -11,13 +11,14 @@ import Header from "./Header";
 import { dispatchHandler } from "../utils";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, defaultStore);
+  const [state, reducerDispatch] = useReducer(reducer, defaultStore);
+  const dispatch = dispatchHandler(reducerDispatch);
 
   return (
     <Store.Provider
       value={{
         state,
-        dispatch: dispatchHandler(dispatch)
+        dispatch
       }}
     >
       <div className="container">
