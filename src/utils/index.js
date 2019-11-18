d5541b5d6a66e8f267fdb62a6f1d6e3dd6ab93a6
Samrith Shankar <samrith@thetotem.co>
2019-11-19 00:03:26 +0100

    Created utils

---
+++ src/utils/index.js
@@ -0,0 +1,30 @@
+import { BASE_URL } from "../data/config";
+
+export async function privateFetch(path, method, data) {
+  const canContainBody = method === "POST";
+  let url = BASE_URL + path;
+
+  if (!canContainBody) {
+    url = new URL(url);
+    Object.entries(params).forEach(([key, value]) =>
+      path.searchParams.append(key, params[key])
+    );
+  }
+
+  const response = await fetch(url, {
+    method,
+    ...(canContainBody
+      ? {
+          body: JSON.stringify(data)
+        }
+      : null)
+  });
+
+  return await response.json();
+}
+
+export function dispatchHandler(dispatch) {
+  return function dispatcher(type, payload = null) {
+    return dispatch({ type, payload });
+  };
+}
