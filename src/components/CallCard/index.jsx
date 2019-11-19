2f2e11068e31839a32825481a75eabcad42ba8f3
Samrith Shankar <samrith@thetotem.co>
2019-11-19 05:16:26 +0100

    Added proper archive management

--- src/components/CallCard/index.jsx
+++ src/components/CallCard/index.jsx
@@ -2,18 +2,41 @@ import React, { useContext, useEffect } from "react";
 
 import { Store } from "../StoreContext";
 
-import { DETAIL_VIEW, ARCHIVE } from "../../data/action";
+import {
+  DETAIL_VIEW,
+  ARCHIVE,
+  ARCHIVE_SUCCESS,
+  ARCHIVE_ERROR
+} from "../../data/action";
 
 import CallCardCompact from "./CallCardCompact";
 import CallCardDetail from "./CallCardDetail";
 
+import fetch from "../../utils/fetch";
+
 export default function CallCard({
-  data: { id, direction, from, to, via, duration, call_type, callTime }
+  data: {
+    id,
+    direction,
+    from,
+    to,
+    via,
+    duration,
+    call_type,
+    callTime,
+    is_archived
+  },
+  callIndex,
+  groupIndex
 }) {
   const {
-    state: { detailView },
+    state: {
+      detailView,
+      archive: { [id]: thisArchive = {} }
+    },
     dispatch
   } = useContext(Store);
+  const { loading, error } = thisArchive;
 
   const isActive = detailView === id;
 
@@ -22,17 +45,32 @@ export default function CallCard({
   }, []);
 
   const handleDetailView = () => {
-    if (!isActive) {
-      dispatch(DETAIL_VIEW, id);
-    } else {
-      dispatch(DETAIL_VIEW, null);
+    if (!loading) {
+      if (!isActive) {
+        dispatch(DETAIL_VIEW, id);
+      } else {
+        dispatch(DETAIL_VIEW, null);
+      }
     }
   };
 
   const handleArchive = e => {
     e.stopPropagation();
-    // dispatch(ARCHIVE, id);
-    // fetch.post(`/`)
+    dispatch(ARCHIVE, id);
+    fetch
+      .post(`/activities/${id}`, {
+        is_archived: !is_archived
+      })
+      .then(() => {
+        dispatch(ARCHIVE_SUCCESS, {
+          callId: id,
+          callIndex,
+          groupIndex,
+          isArchived: !is_archived
+        });
+        dispatch(DETAIL_VIEW, null);
+      })
+      .catch(error => dispatch(ARCHIVE_ERROR, id));
   };
 
   const getAppropriateNumber = (direction, from, to, inverse) =>
@@ -43,8 +81,9 @@ export default function CallCard({
       className="call-card"
       id={id}
       onClick={handleDetailView}
-      data-active={isActive}
-      data-archiving={}
+      data-active={isActive && !loading}
+      data-archiving={loading}
+      data-archived={is_archived}
     >
       <CallCardCompact
         number={getAppropriateNumber(direction, from, to, false)}
