32f62656e2074847345cb947732b17d060536d42
Samrith Shankar <samrith@thetotem.co>
2019-11-19 05:16:38 +0100

    Displayed components accordingly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,36 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import { defaultStore } from "../data/store";
 import reducer from "../data/reducer";
 
+import { FEED, FEED_SUCCESS, FEED_ERROR } from "../data/action";
+
 import { Store } from "./StoreContext";
 
 import Header from "./Header";
 import Feed from "./Feed";
+import FeedLoader from "./Feed/FeedLoader";
+import FeedError from "./Feed/FeedError";
 
+import fetch from "../utils/fetch";
 import { dispatchHandler } from "../utils";
 
 function App() {
   const [state, reducerDispatch] = useReducer(reducer, defaultStore);
   const dispatch = dispatchHandler(reducerDispatch);
+
+  const {
+    feed: { loading, data, error }
+  } = state;
+
+  useEffect(() => {
+    dispatch(FEED);
+    fetch
+      .get("/activities")
+      .then(data => dispatch(FEED_SUCCESS, data))
+      .catch(error => dispatch(FEED_ERROR, error));
+  }, []);
 
   return (
     <Store.Provider
@@ -23,10 +40,16 @@ function App() {
       }}
     >
       <div className="container">
-        <Header />
-        <div className="container-view">
-          <Feed />
-        </div>
+        {loading && <FeedLoader />}
+        {!loading && error && <FeedError />}
+        {!loading && !error && data && (
+          <>
+            <Header />
+            <div className="container-view">
+              <Feed />
+            </div>
+          </>
+        )}
       </div>
     </Store.Provider>
   );
