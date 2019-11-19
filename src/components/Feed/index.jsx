a01d71e455cf66cc52f99454a0a905c346566362
Samrith Shankar <samrith@thetotem.co>
2019-11-19 05:15:53 +0100

    Displayed groups conditionally

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -2,27 +2,15 @@ import React, { useContext, useEffect } from "react";
 
 import { Store } from "../StoreContext";
 
-import { FEED, FEED_SUCCESS, FEED_ERROR } from "../../data/action";
-
 import CallCardGroup from "../CallCard/CallCardGroup";
 
-import fetch from "../../utils/fetch";
-
-export default function Feed(props) {
+export default function Feed() {
   const {
     state: {
-      feed: { loading, error, data }
-    },
-    dispatch
+      feed: { loading, error, data },
+      showArchivedCalls
+    }
   } = useContext(Store);
-
-  useEffect(() => {
-    dispatch(FEED);
-    fetch
-      .get("/activities")
-      .then(data => dispatch(FEED_SUCCESS, data))
-      .catch(error => dispatch(FEED_ERROR, error));
-  }, []);
 
   if (loading) {
     return "Loading..";
@@ -32,7 +20,22 @@ export default function Feed(props) {
     return JSON.stringify(error);
   }
 
-  return data.map(([date, calls]) => (
-    <CallCardGroup key={date} date={date} calls={calls} />
-  ));
+  return data.map(([date, calls], index) => {
+    if (
+      showArchivedCalls ||
+      (!showArchivedCalls && !calls.every(call => call.is_archived))
+    ) {
+      return (
+        <CallCardGroup
+          key={date}
+          date={date}
+          calls={calls}
+          showArchivedCalls={showArchivedCalls}
+          groupIndex={index}
+        />
+      );
+    }
+
+    return null;
+  });
 }
