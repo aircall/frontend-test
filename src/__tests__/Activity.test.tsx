f74b38bde0b6e3c192ee01ba002c3c459c480b3f
Jeremy Scatigna <scatignaj@me.com>
2019-11-21 13:07:05 +0100

    feat(test): add test config and first blank test

---
+++ src/__tests__/Activity.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+import Activity from '../pages/Activity'
+
+describe('<Activity />', () => {
+  test('should display Activity page', async () => {
+    // ???
+  })
+})
