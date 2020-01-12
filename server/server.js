b2248d00015fc9d2273bb8c0012fed613ef5f76d
Kathrin Ayer <kathrin.ayer@gmail.com>
2020-01-12 16:50:23 -0500

    add proxy and cross-origin header handling

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ const path = require('path');
 
 const { aircallActivities, aircallActivitieByID } = require('./routes/aircall');
 
+const port = (process.env.PORT || 3000);
+
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/activities/:id', aircallActivitieByID);
 app.get('/activities', aircallActivities);
@@ -15,4 +22,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3002);
+
+
+app.listen(port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   module: {
@@ -29,6 +30,11 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        DATA_SOURCE: JSON.stringify(process.env.DATA_SOURCE),
+      },
+    }),
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html',
