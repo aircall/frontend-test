0f496125d7491a978153ce31089879b9985f8658
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 04:25:37 +0100

    :sparkles: add selector for Activities

--- src/Redux/Selectors/index.ts
+++ src/Redux/Selectors/index.ts
@@ -1,1 +1,20 @@
-export {};
+import { groupByDate } from "../../utils";
+import { IRootState, IActivity } from "../../types";
+
+export const sortedActivitiesSelector = (state: IRootState) =>
+  Array.from(
+    groupByDate(
+      state.entity.activities.filter(
+        (activity: IActivity) => !activity.is_archived
+      )
+    )
+  );
+
+export const sortedArchivedActivitiesSelector = (state: IRootState) =>
+  Array.from(
+    groupByDate(
+      state.entity.activities.filter(
+        (activity: IActivity) => activity.is_archived
+      )
+    )
+  );
