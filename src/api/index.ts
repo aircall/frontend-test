d43f00af52f027590cccf22e1246303c067288c4
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 05:18:20 +0100

    :sparkles: add handleActivity api call

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,3 +10,15 @@ export const fetchActivityDetail = async (id: string) => {
   const res = await fetch(`${API_URL}/activities/${id}`);
   return (await res.json()) as IActivity;
 };
+
+export const handleActivity = async (id: string, isArchived: boolean) => {
+  const res = await fetch(`${API_URL}/activities/${id}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ is_archived: isArchived })
+  });
+  return await res.json();
+};
