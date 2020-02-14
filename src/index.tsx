a7d58549bc9be0dc1429e8fe2f77cd1d16ce8f9b
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 03:40:47 +0100

    :fire: delete unsed styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./css/body.css";
-import "./css/app.css";
-import "./css/header.css";
 
 import { App } from "./App";
 import { ThemeProvider, theme } from "./StyledComponents";
