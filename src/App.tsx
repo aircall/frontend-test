7a1538a4b460029945344ecf823cc5f61a2b0c56
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 03:12:43 +0100

    :sparkles: :art: add parcel & typescript replace webpack

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import Header from "./Header";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <div className="container-view">Some activities should be here</div>
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./css/body.css";
+import "./css/app.css";
+import "./css/header.css";
+
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
