6365024787a10c6c5fde09ecba7891f6ccedfd93
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 05:04:47 +0100

    :sparkles: add fetchActivityDetail api call

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,3 +5,8 @@ export const fetchActivities = async () => {
   const res = await fetch(`${API_URL}/activities`);
   return (await res.json()) as IActivity[];
 };
+
+export const fetchActivityDetail = async (id: string) => {
+  const res = await fetch(`${API_URL}/activities/${id}`);
+  return (await res.json()) as IActivity;
+};
