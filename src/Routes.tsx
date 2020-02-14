d27290bab6ac2e8dc18f69769913be855103ce8b
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 05:10:39 +0100

    :sparkles: add ActivityDetail Comp to Routes file

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const ActivityFeed = React.lazy(() => import("./components/ActivityFeed"));
+const ActivityDetail = React.lazy(() => import("./components/ActivityDetail"));
 
 export const Routes: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route path="/detail/:id">
-          <h1>Details</h1>
+          <ActivityDetail />
         </Route>
         <ActivityFeed />
       </Switch>
