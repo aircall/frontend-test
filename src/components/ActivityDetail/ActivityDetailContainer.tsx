62f4c57e8d2bd1ce7b0c7bbe4fce25291f1cef3e
soufDev <ait.akkache.soufiane@gmail.com>
2020-02-14 05:10:07 +0100

    :sparkles: add ActivityDetail Comp

---
+++ src/components/ActivityDetail/ActivityDetailContainer.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { fetchActivityDetailAction } from "../../Redux/Actions";
+import { IRootState, IActivity } from "../../types";
+
+type TChildren = {
+  activity: IActivity;
+  goBack: () => void;
+  activityHandler: () => void;
+};
+interface IProps {
+  children: (props: TChildren) => React.ReactNode;
+}
+export const ActivityDetailContainer: React.FC<IProps> = ({ children }) => {
+  const { id } = useParams<{ id: string }>();
+  const history = useHistory();
+  const activity = useSelector(
+    (state: IRootState) => state.entity.activityDetail,
+    shallowEqual
+  );
+  // well I need to make it any too lazy
+  const dispatch = useDispatch() as any;
+
+  const goBack = () => {
+    history.push("/");
+  };
+  const activityHandler = () => {};
+
+  React.useEffect(() => {
+    dispatch(fetchActivityDetailAction(id));
+  }, [id]);
+
+  return <>{children({ activity, activityHandler, goBack })}</>;
+};
