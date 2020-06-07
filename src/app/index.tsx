db80797162d39481e1b2ed8a75631ca2293bf607
Boris Lebon <bo.lebon@gmail.com>
2020-06-07 17:46:07 +0200

    refactor: change file structure

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Header } from './components/header';
+
+export function App() {
+  return (
+    <div className="container">
+      <Header />
+      <div className="container-view">Some activities should be here</div>
+    </div>
+  );
+};
+
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/body.css';
+import './css/app.css';
+import './css/header.css';
+
+import { App } from './app';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
